c8f893c791ed4cc8e96fa18de033f653127a80ce
Alexey-zaliznuak <zaliznuak20@gmail.com>
2025-03-12 21:56:21 +0000

    debug

--- src/services/images.servise.ts
+++ src/services/images.servise.ts
@@ -16,18 +16,20 @@ class ImagesService {
       const { url, fields } = await this.getUploadConfig();
       const formData = new FormData();
 
-      Object.entries(fields).forEach(([key, value]) => {
-        formData.append(key, value);
-      });
+      // Object.entries(fields).forEach(([key, value]) => {
+      //   formData.append(key, value);
+      // });
+      formData.append('fields', JSON.stringify(fields));
 
       formData.append('file', file);
 
-      const response = await fetch(url, {
-        method: 'POST',
-        body: formData,
+      const response = await instance.post(url, formData, {
+        headers: {
+          'Content-Type': 'image/*',
+        },
       });
 
-      if (!response.ok) {
+      if (response.status > 300) {
         throw new Error('Ошибка при загрузке файла');
       }
 
